327cc727d8c5234d2bbbeca0130e46cb843e1971
wafaofficial09-dev <wafaofficial09@gmail.com>
2026-06-24 15:51:14 +0530

    Create sw.js

---
+++ artifacts/storefront/public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "shopeasy-pwa-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/manifest.json",
+        "/favicon.svg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || caches.match("/");
+      });
+    })
+  );
+});
